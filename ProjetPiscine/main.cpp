95c30f72750ad177da250671e523e84d6df0787f
EliottElek <64375473+EliottElek@users.noreply.github.com>
2020-04-26 22:34:03 +0200

    Update main.cpp
    chagements dans le main

--- ProjetPiscine/main.cpp
+++ ProjetPiscine/main.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Capu et Sube!" << endl;
+    int nombre1 = 0;
+    int nombre2 = 0;
+    int nombre3 = 0;
+    cout << "Saisissez le nombre 1 :" << endl;
+    cout << "Saisissez le nombre 2 :" << endl;
+    cin >> nombre1;
+    cin >> nombre2;
     return 0;
 }
