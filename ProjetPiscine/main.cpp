25a76b9cc7b00c452d04113f6848e8d9d12999a5
EliottElek <64375473+EliottElek@users.noreply.github.com>
2020-04-26 22:51:56 +0200

    j'ai rajouté chef

--- ProjetPiscine/main.cpp
+++ ProjetPiscine/main.cpp
@@ -4,7 +4,9 @@ using namespace std;
 
 int main()
 {
+    //bizarre ce truc
     int nombre1 = 0;
+    int chef;
     int nombre2 = 0;
     int nombre3 = 0;
     cout << "Saisissez le nombre 1 :" << endl;
